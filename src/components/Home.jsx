c615c33640f982b5198cf978b252d86d102529bf
irishmaiden18 <laurapohl18@gmail.com>
2026-04-30 10:11:52 -0400

    adds filters to productList

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,37 @@ const Home = () => {
 
     const {productList} = useContext(ProductListContext)
 
+    const [filterBy, setFilterBy] = useState("")
+
+    const filterProducts = (filteredBy) => {
+        const filteredProducts = productList.filter((product) => {
+            return product.category.slug === filteredBy
+        })
+        return filteredProducts
+    }
+
+    const filteredProducts = () => {
+        if (filterBy === "electronics") {
+            return filterProducts("electronics")
+        } else if (filterBy === "furniture") {
+            console.log("furniture")
+            return filterProducts("furniture")
+        } else {
+            return productList
+        }
+    }
+
+    let filteredProductList = filteredProducts()
+
     return (
         <>
             <h2>Home</h2>
+            <button onClick={() => setFilterBy("electronics")}>Electronics</button>
+            <button onClick={() => setFilterBy("furniture")}>Furniture</button>
+            <button onClick={() => setFilterBy("")}>All Products</button>
             <ul>
-                {productList ? (
-                    productList.map((product) => (
+                {filteredProductList ? (
+                    filteredProductList.map((product) => (
                         <li key={product.id}>
                             <MiniProductCard product={product}/>
                         </li>
